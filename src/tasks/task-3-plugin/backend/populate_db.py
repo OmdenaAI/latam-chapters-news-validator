36ea471946730e886d55f96d98228b0dbee00b47
Isaacrodgzb <isaac.rodriguez@botco.ai>
2022-10-04 09:31:31 -0500

    Add comments

--- src/tasks/task-3-plugin/backend/populate_db.py
+++ src/tasks/task-3-plugin/backend/populate_db.py
@@ -4,16 +4,22 @@ from deta import Deta
 from datetime import datetime
 import os
 
+# Make connection to Deta databases
+
 deta = Deta(os.environ['PROJECT_KEY'])
 web_article_db = deta.Base("web-article-db")
 web_site_db = deta.Base("website-db")
 
+# Helper function to extract URL domain
+
 def get_url_domain(url: str):
     return urlparse(url).netloc
 
-data_train = pd.read_excel("~/Downloads/train.xlsx")
-data_dev = pd.read_excel("~/Downloads/development.xlsx")
-data_test = pd.read_excel("~/Downloads/test.xlsx")
+# Load fake news dataset (https://github.com/jpposadas/FakeNewsCorpusSpanish)
+
+data_train = pd.read_excel("./train.xlsx")
+data_dev = pd.read_excel("./development.xlsx")
+data_test = pd.read_excel("./test.xlsx")
 
 data = pd.concat([
     data_train,
@@ -21,11 +27,17 @@ data = pd.concat([
     data_test
 ]).reset_index()
 
+# Keep only the relevant columns
+
 data =  data[['Category', 'Link', 'Source']]
 data = data[~data.Link.isnull()] # remove nulls
 
+# Extract website domain in new column 'Domain'
+
 data['Domain'] = data['Link'].apply(get_url_domain)
 data = data[data['Category'] == 'Fake']
+
+# Reset website table in case of re-population
 
 for row in data.to_numpy():
     web_site_db.update(
@@ -33,11 +45,13 @@ for row in data.to_numpy():
             "count": 0
         }, 
         key = row[3]
-    ) 
+    )
+
+# Process and save each news article in Deta databases
 
 for row in data.to_numpy():
     try:
-        print(row)
+        # Save web article
         timestamp = datetime.utcnow().strftime('%Y-%m-%d.%H:%M:%S')
         web_article_result = web_article_db.put(
             data = {
@@ -48,7 +62,7 @@ for row in data.to_numpy():
             },
             key = row[1]
         )
-
+        # Update news website statistics (how many fake articles come from this domain)
         web_site_result = web_site_db.get(key = row[3])
         if web_site_result is None:
             web_site_db.put(
